6343c61c5202f158f6a29101ad3729f0daee2da1
idriss <idrissilhan@gmail.com>
2024-06-21 22:36:18 +0800

    Libft update so far

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -15,24 +15,59 @@
 #include <stdlib.h>
 
 char    *ft_strtrim(char const *s1, char const *set);
+int     is_char_there(char w, char const *word);
+
+int     is_char_there(char w, char const *word)
+{
+    size_t count;
+
+    count = 0;
+    while (word[count] != '\0')
+    {
+        if (word[count] == w)
+        {
+            return (1);
+        }
+        count++;
+    }
+    return (0);
+}
 
 char    *ft_strtrim(char const *s1, char const *set)
 {
-    char *str;
-    size_t count;
+    size_t count1; //Start of string
+    size_t count2; //End of string
+    size_t i;
+    char   *str;
 
-    while ((s1[count] != '\0') && (set[count] != '\0'))
+    if (s1 == NULL)
+        return (NULL);
+    while (s1[count1] != '\0' && is_char_there(s1[count1], set))
     {
-        if (s1[count] == set[count])
-            count++;
-        count++;
+        count1++;
     }
+    count2 = ft_strlen(s1);
+    while ((count1 < count2) && is_char_there(s1[count2 - 1], set))
+    {
+        count2--;
+    }
+    str = (char *) malloc((count2 - count1 + 1) * sizeof(*s1));
+    if (str == NULL)
+        return (NULL);
+    while (count1 < count2)
+    {
+        str[i] = s1[count1];
+        i++;
+        count1++;
+    }
+    str[i] = '\0';
+    return (str);
 }
-
+/*
 int main()
 {
     char *result;
-    result = ft_strtrim("Hello World!", "World!");
+    result = ft_strtrim(" Hello World! ", " ");
     if (result != NULL)
     {
         printf("%s\n", result);
@@ -44,3 +79,4 @@ int main()
     }
     return (0);
 }
+*/
